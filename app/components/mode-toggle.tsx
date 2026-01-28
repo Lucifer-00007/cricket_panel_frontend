e1e13e8cdbb2f8a6358358b6cae3920f36ed4e39
LUCI <ani.mimansatech@gmail.com>
2026-01-28 20:11:53 +0530

    Update mode-toggle.tsx

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -18,13 +18,13 @@ export function ModeToggle() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon" className="dark:border-white/20">
+                <Button variant="outline" size="icon" className="dark:border-white/30 dark:hover:bg-primary/30">
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="bg-popover dark:bg-popover border-border dark:border-white/[0.15] min-w-[120px]">
+            <DropdownMenuContent align="end" className="bg-popover dark:bg-primary/70 border-border dark:border-white/20 min-w-[120px]">
                 <DropdownMenuItem onClick={() => setTheme("light")} className="cursor-pointer dark:text-white dark:hover:bg-white/[0.08] dark:focus:bg-white/[0.08]">
                     Light
                 </DropdownMenuItem>
